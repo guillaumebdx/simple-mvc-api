c99e5365dc624fbd5171536d746e8cb05b8f3c02
chaem16028 <chaem16028@gmail.com>
2020-04-29 20:53:58 +0200

    ajout lien nav

---
+++ src/Controller/RandomController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+class RandomController extends AbstractController
+{
+    public function index()
+    {
+        return $this->twig->render('Random/random.html.twig');
+    }
+    public function picture()
+    {
+        $url = 'https://api.windy.com/api/webcams/v2/list/orderby=random';
+        $videoData = $this->get($url.'?show=webcams:player&key=' . APP_API_KEY);
+        $paragrapheData = $this->get($url.'?show=webcams:location&key=' .APP_API_KEY);
+        $para = $paragrapheData['result']['webcams'][0];
+        //$data3 = 'https://webcams.windy.com/webcams/stream/'.$id;
+        $data = $videoData['result']['webcams'][0]['player']['live'];
+        $data2 = $videoData['result']['webcams'][0]['player']['day'];
+
+        if ($data['available']==true) {
+            return $this->twig->render('Random/random.html.twig', [
+                'data' => $data,
+                'para' => $para['location'],
+            ]);
+        } else {
+            return $this->twig->render('Random/random.html.twig', [
+                'data' => $data2['embed'],
+                'para' => $para['location'],
+            ]);
+        }
+    }
+}
