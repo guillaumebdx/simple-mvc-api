11af1eb7d88c3e85bd8fb421aa44092cec2a3761
Alexis <Alexis.perront@hotmail.fr>
2020-04-29 22:44:52 +0200

    modification css

--- src/Controller/RandomController.php
+++ src/Controller/RandomController.php
@@ -13,20 +13,23 @@ class RandomController extends AbstractController
         $url = 'https://api.windy.com/api/webcams/v2/list/orderby=random';
         $videoData = $this->get($url.'?show=webcams:player&key=' . APP_API_KEY);
         $paragrapheData = $this->get($url.'?show=webcams:location&key=' .APP_API_KEY);
+        $viewData = $this->get($url.'?show=webcams:location,statistics&key=' .APP_API_KEY);
+        $view = $viewData['result']['webcams'][0]['statistics'];
         $para = $paragrapheData['result']['webcams'][0];
         //$data3 = 'https://webcams.windy.com/webcams/stream/'.$id;
         $data = $videoData['result']['webcams'][0]['player']['live'];
         $data2 = $videoData['result']['webcams'][0]['player']['day'];
-
         if ($data['available']==true) {
             return $this->twig->render('Random/random.html.twig', [
                 'data' => $data,
                 'para' => $para['location'],
+                'view' => $view['views'],
             ]);
         } else {
             return $this->twig->render('Random/random.html.twig', [
                 'data' => $data2['embed'],
                 'para' => $para['location'],
+                'view' => $view['views'],
             ]);
         }
     }
--- src/Controller/VideoController.php
+++ src/Controller/VideoController.php
@@ -12,7 +12,10 @@ class VideoController extends AbstractController
         $url = 'https://api.windy.com/api/webcams/v2/list/webcam=';
         $videoData = $this->get($url.$id.'?show=webcams:player&key=' . APP_API_KEY);
         $paragrapheData = $this->get($url.$id.'?show=webcams:location&key=' .APP_API_KEY);
+        $viewData = $this->get($url.$id.'?show=webcams:location,statistics&key=' .APP_API_KEY);
+        $view = $viewData['result']['webcams'][0]['statistics'];
         $para = $paragrapheData['result']['webcams'][0];
+
         $data3 = 'https://webcams.windy.com/webcams/stream/'.$id;
         $data = $videoData['result']['webcams'][0]['player']['live'];
         $data2 = $videoData['result']['webcams'][0]['player']['day'];
@@ -21,11 +24,13 @@ class VideoController extends AbstractController
             return $this->twig->render('Video/index.html.twig', [
                 'data' => $data3,
                 'para' => $para['location'],
+                'view' => $view['views'],
             ]);
         } else {
             return $this->twig->render('Video/index.html.twig', [
                 'data' => $data2['embed'],
                 'para' => $para['location'],
+                'view' => $view['views'],
             ]);
         }
     }
